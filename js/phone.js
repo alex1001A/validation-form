5784ea5e3d944819a0a939639311da39f771d773
alex1001A <alexiordanov1998@gmail.com>
2024-07-07 17:23:03 +0300

    add chosing phone code of the country

---
+++ js/phone.js
@@ -0,0 +1,11 @@
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  initialCountry: "auto",
+  geoIpLookup: function (callback) {
+    fetch("https://ipinfo.io?token=ipinfo.io/91.238.23.39?token=09bed92e4a94ed", { headers: { Accept: "application/json" } })
+      .then((response) => response.json())
+      .then((data) => callback(data.country))
+      .catch(() => callback("ua"));
+  },
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/utils.js",
+});
